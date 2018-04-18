3454566cd27487eebe27232a3e82bb7b608eb013
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-04-18 12:35:09 +0100

    - Kata Complete
    - Added Unit tests
    - Added Solution

--- src/main/scala/SumOfStrings.scala
+++ src/main/scala/SumOfStrings.scala
@@ -1,5 +1,11 @@
 object SumOfStrings extends App {
 
-  def singleSum (numberList : List[Int]) : Int = 0
+  def singleSum (numberList : List[Int]) : Int = {
 
+    val removeDuplicates = numberList.distinct
+    val pullDupes = numberList.diff(removeDuplicates)
+    val pullNonDupes = removeDuplicates.diff(pullDupes)
+
+    pullNonDupes.sum
+  }
 }
